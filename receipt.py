58d8ed44997e367545e5cfc0dc5c9b7a5cdd5163
aman-agarwal35 <65167075+aman-agarwal35@users.noreply.github.com>
2022-06-18 11:06:29 +0530

    new changes

---
+++ receipt.py
@@ -0,0 +1,18 @@
+sample= f'''\tCompany Name, XYZ\n\t\Adress: XYZ, Floor x
+
+--------------------------------------------------------
+Employee ID\t\t: 
+Salary Of\t\t: Mon- YYYY
+Generated On\t\t: DD-MM-YYYY
+--------------------------------------------------------
+
+Basic salary\t\t: Rs.------
+PF\t\t: Rs.------
+HRA\t\t: Rs.------
+Allowances\t\t: Rs.------
+Income Tax\t\t: Rs.------
+Net Salary\t\t: Rs.------
+
+---------------------------------------------------------
+
+  '''
